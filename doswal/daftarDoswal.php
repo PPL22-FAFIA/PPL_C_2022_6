960580109686c5f227f941b4d0ccde582460396c
Muhammad Farhan Haris Rosidi <farhanharis66@gmail.com>
2022-10-06 21:38:10 +0700

    bikin filter di  list mahasiswa dosen wali

--- doswal/daftarDoswal.php
+++ doswal/daftarDoswal.php
@@ -1,56 +1,89 @@
-<?php 
-    require_once('../bootstrap/header.html');
-    require_once('../lib/db_login.php'); 
+<?php
+require_once('../bootstrap/header.html');
+require_once('../lib/db_login.php');
 ?>
 <div class="row g-0">
-    <div class="col-2">
-        <?php require_once '../dashboard/sidebarDoswal.php' ?>
+  <div class="col-2">
+    <?php require_once '../dashboard/sidebarDoswal.php' ?>
+
+  </div>
+  <div class="col d-flex flex-column">
+    <h3 class="">Daftar Mahasiswa</h3>
+    <div class="d-flex flex-row w-100">
+      <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" class="f">
+      <button class="btn btn-outline-secondary" type="button" data-toggle="modal" data-target="#modalFilter">Filter</button>
+      <input type="text" class="form-control border w-25" id="bigger-smaller" placeholder="> / <">
+      <input type="text" class="form-control w-25" id="angkatan" placeholder="Angkatan...">
+      <select class="form-select w-25" aria-label="Default select example">
+        <option selected>Status PKL</option>
+        <option value="1">Sudah Ambil</option>
+        <option value="2">Belum Ambil</option>
+      </select>
+      <select class="form-select w-25" aria-label="Default select example">
+        <option selected>Status SkripsiL</option>
+        <option value="1">Sudah Ambil</option>
+        <option value="2">Belum Ambil</option>
+      </select>
 
     </div>
-    <div class="col d-flex flex-column">
-        <h3 class="">Daftar Mahasiswa</h3>
-        <div class="d-flex flex-row col-3">
-            <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                aria-describedby="button-addon2" class="f">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Filter</button>
-        </div>
-        <div class="d-flex flex-row-reverse">
-            <!-- print table from database siap1 -->
-            <?php
-                $query = "SELECT m.Nim, m.Nama AS nama_mhs, m.status AS status_mhs, m.Angkatan AS angkatan_mhs FROM tb_mhs m";
-                $result = $db->query($query);
-                if (!$result) {
-                    die ("Could not query the database: <br>".$db->error."<br>Query: ".$query);
-                }
-                $i = 1;
-                echo '<table class="table table-striped table-hover">';
-                echo '<thead>';
-                echo '<tr>';
-                echo '<th scope="col">No</th>';
-                echo '<th scope="col">NIM</th>';
-                echo '<th scope="col">Nama</th>';
-                echo '<th scope="col">NIM</th>';
-                echo '<th scope="col">Angkatan</th>';
-                echo '<th scope="col">Rincian Studi</th>';
-                echo '</tr>';
-                echo '</thead>';
-                echo '<tbody>';
-                while ($row = $result->fetch_object()) {
-                    echo '<tr>';
-                    echo '<th scope="row">'.$i.'</th>';
-                    echo '<td>'.$row->Nim.'</td>';
-                    echo '<td>'.$row->nama_mhs.'</td>';
-                    echo '<td>'.$row->status_mhs.'</td>';
-                    echo '<td>'.$row->angkatan_mhs.'</td>';
-                    echo '<td><a href="detailMhs.php?nim='.$row->Nim.'" class="btn btn-primary">Detail</a></td>';
-                    echo '</tr>';
-                    $i++;
-                }
-                echo '</tbody>';
-                echo '</table>';
-                $result->free();
-                $db->close();
-            ?>
-        </div>
+    <div class="d-flex flex-row-reverse">
+      <!-- print table from database siap1 -->
+      <?php
+      $query = "SELECT m.Nim, m.Nama AS nama_mhs, m.status AS status_mhs, m.Angkatan AS angkatan_mhs FROM tb_mhs m";
+      $result = $db->query($query);
+      if (!$result) {
+        die("Could not query the database: <br>" . $db->error . "<br>Query: " . $query);
+      }
+      $i = 1;
+      echo '<table class="table table-striped table-hover">';
+      echo '<thead>';
+      echo '<tr>';
+      echo '<th scope="col">No</th>';
+      echo '<th scope="col">NIM</th>';
+      echo '<th scope="col">Nama</th>';
+      echo '<th scope="col">NIM</th>';
+      echo '<th scope="col">Angkatan</th>';
+      echo '<th scope="col">Rincian Studi</th>';
+      echo '</tr>';
+      echo '</thead>';
+      echo '<tbody>';
+      while ($row = $result->fetch_object()) {
+        echo '<tr>';
+        echo '<th scope="row">' . $i . '</th>';
+        echo '<td>' . $row->Nim . '</td>';
+        echo '<td>' . $row->nama_mhs . '</td>';
+        echo '<td>' . $row->status_mhs . '</td>';
+        echo '<td>' . $row->angkatan_mhs . '</td>';
+        echo '<td><a href="detailMhs.php?nim=' . $row->Nim . '" class="btn btn-primary">Detail</a></td>';
+        echo '</tr>';
+        $i++;
+      }
+      echo '</tbody>';
+      echo '</table>';
+      $result->free();
+      $db->close();
+      ?>
     </div>
+  </div>
+</div>
+
+<!-- MODAL -->
+<div class="modal fade" id="modalFilter" tabindex="-1" role="dialog" aria-labelledby="modalFilterTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalFilterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        amdksmdksmd
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
