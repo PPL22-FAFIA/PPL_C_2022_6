cbc77e744eb07fc99ba03fef6922d9a4f1aa472f
Farrel Andhika Rizky Putra <farrelarp@students.undip.ac.id>
2022-10-06 13:35:59 +0700

    nyambung db mahasiswa

--- auth/login.php
+++ auth/login.php
@@ -25,38 +25,9 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-<!-- login form in center of page-->
-<!-- <div class="box">
-    <div class="row d-flex align-items-center text-center ms-auto justify-content-center">
-        <div class="col-4">
-            <div class="card">
-                <div class="card-header">
-                    <h1 class="text-center">Login</h1>
-                </div>
-                <div class="card-body">
-                    <form action="login.php" method="post">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" placeholder="Username" value="<?php if (isset($_POST["username"])) echo $username ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                        </div>
-                        <?php if (isset($error)) : ?>
-                            <div class="alert alert-danger mt-3" role="alert">
-                                <?php echo $error; ?>
-                            </div>
-                        <?php endif; ?>
-                        <div class="text-center mt-3">
-                            <button type="submit" name="submit" value="submit" class="btn btn-primary">Login</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> -->
 <div class="app-content content ">
+    <br>
+    <br>
     <div class="content-wrapper">
         <div class="content-header row">
         </div>
@@ -65,14 +36,12 @@ if (isset($_POST['submit'])) {
                 <div class="col-12 d-flex align-items-center justify-content-center">
                     <div class="col-xl-4 col-lg-6 col-md-12 col-sm-12 box-shadow-2 p-0">
                         <div class="card border-grey border-lighten-3 px-1 py-1 m-0">
-
                             <div class="card-header border-0 pb-0">
                                 <div class="card-title text-center">
                                     <img src="https://sso.undip.ac.id/assets/app/images/logo-undip.png" alt="Universitas Diponegoro" style="width: 150px;">
                                     <h1><strong>SIAP</strong></h1>
                                     <h2>Universitas Diponegoro</h2>
                                 </div>
-                                <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2"><span>Silahkan Masuk</span></h6>
                             </div>
                             <div class="card-content">
                                 <div class="card-body">
@@ -80,7 +49,7 @@ if (isset($_POST['submit'])) {
                                     <div class="row">
                                         <div class="col-lg-12 col-md-12 col-xs-12 mb-1">
                                             <marquee onmouseover="this.stop();" onmouseout="this.start();">
-                                                Berita : <a href="https://sso.undip.id" target="_blank" class="red"><u><b>TRYOUT UNDIP</b> dapat diakses di sini .</u></a> atau link : <a href="https://sso.undip.id" target="_blank" class="red"><u>https://sso.undip.id</u></a>
+                                                Selamat Datang di Sistem Informasi Akademik Informatika Universitas Diponegoro
                                             </marquee>
                                         </div>
                                     </div>
--- dashboard/dashboardMhs.php
+++ dashboard/dashboardMhs.php
@@ -1,81 +1,98 @@
-<?php require_once("../lib/db_login.php");?>
+<?php include_once '../lib/db_login.php'; 
+$nim = $_SESSION['user']['Nim_Nip'];
+$query = "SELECT * FROM tb_mhs WHERE Nim = '$nim'";
+// execute query
+$result = mysqli_query($db, $query);
+// check result
+    if (mysqli_num_rows($result) > 0) {
+        // get data
+        $data = mysqli_fetch_assoc($result);
+        // set session
+        $_SESSION['dataMhs'] = $data;
+    } 
+
+?>
 <div class="container">
-        <h1 class="d-flex justify-content-center">Dashboard Mahasiswa</h1>
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Status Akademik</h4>
-                        <p class="card-text">Dosen Wali</p>
-                        <p class="card-text">NIP</p>
-                        <div class="row text-center">
-                            <div class="col">Semester</div>
-                            <div class="col">Status</div>
-                        </div>
-                        <div class="row text-center">
-                            <h1 class="col">5</h1>
-                            <h3 class="col badge fs-4 text-bg-success text-white">Aktif</h3>
-                        </div>
+    <h1 class="d-flex justify-content-center mt-3">Dashboard Mahasiswa</h1>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Status Akademik</h4>
+                    <p class="card-text">Dosen Wali</p>
+                    <p class="card-text">NIP</p>
+                    <div class="row text-center">
+                        <div class="col">Semester</div>
+                        <div class="col">Status</div>
                     </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" width="150" alt="gene">
-                        <h3 class="m-0">Farrel</h3>
-                        <p class="m-0">NIM</p>
-                        <p class="m-0">Dosen</p>
+                    <div class="row text-center">
+                        <h1 class="col">5</h1>
+                        <h3 class="col badge fs-4 text-bg-success text-white">Aktif</h3>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row mt-3">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h4>Data Mahasiswa</h4>
-                        <div class="mt-3">
-                            <p class="m-0">Nama Lengkap</p>
-                            <p class="fw-bold m-0">Abyan</p>
-                        </div>
-                        <div class="mt-3">
-                            <p class="m-0">NIM</p>
-                            <p class="fw-bold m-0">05111940000001</p>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <!-- anchor editProfil -->
-                            <a href="../mahasiswa/editProfilMhsPage.php" class="btn btn-primary mt-3">Edit Profil</a>
-                        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body text-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" width="150" alt="gene">
+                    <h3 class="m-0"><?php echo $_SESSION["dataMhs"]["Nama"]?></h3>
+                    <p class="m-0"><?php echo $_SESSION["dataMhs"]["Nim"]?></p>
+                    <p class="m-0">Mahasiswa</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4>Data Mahasiswa</h4>
+                    <div class="mt-3">
+                        <p class="m-0">Nama Lengkap</p>
+                        <p class="fw-bold m-0"><?php echo $_SESSION["dataMhs"]["Nama"]?></p>
+                    </div>
+                    <div class="mt-3">
+                        <p class="m-0">NIM</p>
+                        <p class="fw-bold m-0"><?php echo $_SESSION["dataMhs"]["Nim"]?></p>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <!-- anchor editProfil -->
+                        <a href="../mahasiswa/editProfilMhsPage.php" class="btn btn-primary mt-3">Edit Profil</a>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Prestasi Akademik</h5>
-                        <div class="row">
-                            <div class="col">
-                                <p class="text-center m-0">SKS semester</p>
-                                <h3 class="text-center">93</h3>
-                            </div>
-                            <div class="col">
-                                <p class="text-center m-0">SKS Kumulatif</p>
-                                <h3 class="text-center">93</h3>
-                            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Prestasi Akademik</h5>
+                    <div class="row">
+                        <div class="col">
+                            <p class="text-center m-0">SKS semester</p>
+                            <h3 class="text-center">93</h3>
                         </div>
-                        <div class="row">
-                            <div class="col">
-                                <p class="text-center m-0">IP semester</p>
-                                <h3 class="text-center">93</h3>
-                            </div>
-                            <div class="col">
-                                <p class="text-center m-0">IP Kumulatif</p>
-                                <h3 class="text-center">93</h3>
-                            </div>
+                        <div class="col">
+                            <p class="text-center m-0">SKS Kumulatif</p>
+                            <h3 class="text-center">93</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <p class="text-center m-0">IP semester</p>
+                            <h3 class="text-center">93</h3>
+                        </div>
+                        <div class="col">
+                            <p class="text-center m-0">IP Kumulatif</p>
+                            <h3 class="text-center">93</h3>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<script>
+    const namaLengkap = document.getElementById('namaLengkap');
+</script>
