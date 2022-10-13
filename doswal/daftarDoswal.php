6f79a95c9bca58861e4af4cae141b6f8d2d58147
Muhammad Farhan Haris Rosidi <farhanharis66@gmail.com>
2022-10-13 18:17:04 +0700

    bikin show table

--- doswal/daftarDoswal.php
+++ doswal/daftarDoswal.php
@@ -9,80 +9,66 @@ $nip = $_SESSION['user']['Nim_Nip'];
     <?php require_once '../dashboard/sidebarDoswal.php' ?>
 
   </div>
-  <div class="col ">
+  <div class="col mx-3">
     <h3 class="">Daftar Mahasiswa</h3>
     <div class="w-100">
       <input type="text" class="form-control w-25" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
-      <div class="d-flex flex-row">
+      <div class="d-flex flex-row justify-content-around">
         <div class="d-flex justify-content-between">
-          <div>
-            <input type="text" class="form-control border w-50" id="bigger-smaller" placeholder="> / <">
-          </div>
-          <div>
-            <input type="text" class="form-control w-50" id="angkatan" placeholder="Angkatan...">
-          </div>
-          <div>
-            <select class="form-select w-50" aria-label="Default select example">
-              <option selected>Status PKL</option>
-              <option value="1">Sudah Ambil</option>
-              <option value="2">Belum Ambil</option>
-            </select>
-          </div>
-          <div>
-            <select class="form-select w-50" aria-label="Default select example">
-              <option selected>Status Skripsi</option>
-              <option value="1">Sudah Ambil</option>
-              <option value="2">Belum Ambil</option>
-            </select>
-          </div>
+          <input type="text" class="form-control border w-100 mx-5" id="bigger-smaller" placeholder="> / <">
+          <input type="text" class="form-control w-100 mx-5" id="angkatan" placeholder="Angkatan...">
+          <select class="form-select w-100 mx-5" aria-label="Default select example">
+            <option selected>Status PKL</option>
+            <option value="1">Sudah Ambil</option>
+            <option value="2">Belum Ambil</option>
+          </select>
+          <select class="form-select w-100 mx-5" aria-label="Default select example">
+            <option selected>Status Skripsi</option>
+            <option value="1">Sudah Ambil</option>
+            <option value="2">Belum Ambil</option>
+          </select>
         </div>
       </div>
     </div>
     <div class="col d-flex flex-column">
-        <h3 class="">Daftar Mahasiswa</h3>
-        <div class="d-flex flex-row col-3">
-            <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                aria-describedby="button-addon2" class="f">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Filter</button>
-        </div>
-        <div class="d-flex flex-row-reverse">
-            <!-- print table from database siap1 -->
-            <?php
-                $query = "SELECT m.Nim, m.Nama AS nama_mhs, m.status AS status_mhs, m.Angkatan AS angkatan_mhs FROM tb_mhs m JOIN tb_dosen d WHERE m.Kode_Wali = d.Kode_Wali AND d.Nip = '$nip'";
-                $result = $db->query($query);
-                if (!$result) {
-                    die ("Could not query the database: <br>".$db->error."<br>Query: ".$query);
-                }
-                $i = 1;
-                echo '<table class="table table-striped table-hover">';
-                echo '<thead>';
-                echo '<tr>';
-                echo '<th scope="col">No</th>';
-                echo '<th scope="col">NIM</th>';
-                echo '<th scope="col">Nama</th>';
-                echo '<th scope="col">NIM</th>';
-                echo '<th scope="col">Angkatan</th>';
-                echo '<th scope="col">Rincian Studi</th>';
-                echo '</tr>';
-                echo '</thead>';
-                echo '<tbody>';
-                while ($row = $result->fetch_object()) {
-                    echo '<tr>';
-                    echo '<th scope="row">'.$i.'</th>';
-                    echo '<td>'.$row->Nim.'</td>';
-                    echo '<td>'.$row->nama_mhs.'</td>';
-                    echo '<td>'.$row->status_mhs.'</td>';
-                    echo '<td>'.$row->angkatan_mhs.'</td>';
-                    echo '<td><a href="../all/detailMhs.php?nim='.$row->Nim.'" class="btn btn-primary">Detail</a></td>';
-                    echo '</tr>';
-                    $i++;
-                }
-                echo '</tbody>';
-                echo '</table>';
-                $result->free();
-                $db->close();
-            ?>
-        </div>
+      <div class="d-flex flex-row-reverse">
+        <!-- print table from database siap1 -->
+        <?php
+        $query = "SELECT m.Nim, m.Nama AS nama_mhs, m.status AS status_mhs, m.Angkatan AS angkatan_mhs FROM tb_mhs m JOIN tb_dosen d WHERE m.Kode_Wali = d.Kode_Wali AND d.Nip = '$nip'";
+        $result = $db->query($query);
+        if (!$result) {
+          die("Could not query the database: <br>" . $db->error . "<br>Query: " . $query);
+        }
+        $i = 1;
+        echo '<table class="table table-striped table-hover">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th scope="col">No</th>';
+        echo '<th scope="col">NIM</th>';
+        echo '<th scope="col">Nama</th>';
+        echo '<th scope="col">NIM</th>';
+        echo '<th scope="col">Angkatan</th>';
+        echo '<th scope="col">Rincian Studi</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        while ($row = $result->fetch_object()) {
+          echo '<tr>';
+          echo '<th scope="row">' . $i . '</th>';
+          echo '<td>' . $row->Nim . '</td>';
+          echo '<td>' . $row->nama_mhs . '</td>';
+          echo '<td>' . $row->status_mhs . '</td>';
+          echo '<td>' . $row->angkatan_mhs . '</td>';
+          echo '<td><a href="../all/detailMhs.php?nim=' . $row->Nim . '" class="btn btn-primary">Detail</a></td>';
+          echo '</tr>';
+          $i++;
+        }
+        echo '</tbody>';
+        echo '</table>';
+        $result->free();
+        $db->close();
+        ?>
+      </div>
     </div>
   </div>
 </div>
--- function/show_nilai.php
+++ function/show_nilai.php
@@ -1,36 +1,38 @@
-<?php 
-    require_once('../lib/db_login.php'); 
+<?php
+require_once('../lib/db_login.php');
 
-    if (isset($_GET['smt'])){
-        $nim = $_GET['nim'];
-        $smt = $_GET['smt'];
-        
-        $query = " SELECT k.Nama as nama_mk, n.Nilai as nilai, k.SKS as sks FROM tb_nilai n JOIN tb_mhs m JOIN tb_matkul k WHERE n.Nim = m.Nim AND n.Kode_Matkul = k.Kode AND m.Nim = '".$nim."' AND n.Semester = '".$smt."' ";
-        $result = $db->query($query);
-        if (!$result) {
-            die("Could not query the database: <br />" . $db->error . "<br>Query: " . $query);
-        }
+if (isset($_GET['smt'])) {
+    $nim = $_GET['nim'];
+    $smt = $_GET['smt'];
 
-        // Execute the query 
-        // Fetch and display the results 
-        echo("<table>");
-        echo("<tr>");
-        echo("<th>Mata Kuliah</th>");
-        echo("<th>Nilai</th>");
-        echo("<th>SKS</th>");
-        echo("</tr>");
-        while ($row = $result->fetch_object()) {
-            echo '<tr>';
-            echo '<td>' . $row->nama_mk . '</td>';
-            echo '<td>' . $row->nilai . '</td>';
-            echo '<td>' . $row->sks . '</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    
-        $result->free();
-        $db->close();
+    $query = " SELECT k.Nama as nama_mk, n.Nilai as nilai, k.SKS as sks FROM tb_nilai n JOIN tb_mhs m JOIN tb_matkul k WHERE n.Nim = m.Nim AND n.Kode_Matkul = k.Kode AND m.Nim = '" . $nim . "' AND n.Semester = '" . $smt . "' ";
+    $result = $db->query($query);
+    if (!$result) {
+        die("Could not query the database: <br />" . $db->error . "<br>Query: " . $query);
     }
-
-    
 ?>
+        <table class="table table-borderless text-center">
+            <thead>
+                <th scope="col" class="h5">No</th>
+                <th scope="col" class="h5">Mata Kuliah</th>
+                <th scope="col" class="h5">Nilai</th>
+                <th scope="col" class="h5">SKS</th>
+            </thead>
+            <tbody>
+            <?php
+            $i = 1;
+            while ($row = $result->fetch_object()) {
+                echo '<tr>';
+                echo '<th>' . $i . '</th>';
+                echo '<td>' . $row->nama_mk . '</td>';
+                echo '<td>' . $row->nilai . '</td>';
+                echo '<td>' . $row->sks . '</td>';
+                echo '</tr>';
+                $i++;
+            }
+            $result->free();
+            $db->close();
+        }
+            ?>
+            </tbody>
+        </table>
