392260359a69c45ce238b303fb59e9f32c0d1cd3
farrel216 <farrel.arp@gmail.com>
2022-11-03 06:57:26 +0700

    finish pkl skripsi

--- function/edit_pkl.php
+++ function/edit_pkl.php
@@ -1,11 +1,11 @@
-<!-- edit profile php ajax -->
 <?php
 session_start();
 require_once('../lib/db_login.php');
     $tempat = $_POST['tempat'];
+    $nilai = $_POST['nilai'];
     $status = $_POST['status'];
     $nim = $_SESSION["user"]["Nim_Nip"];
-    $query = "UPDATE tb_pkl SET Tempat='$tempat', Status ='$status' WHERE Nim='$nim'";
+    $query = "UPDATE tb_pkl SET Tempat='$tempat', Status ='$status', Nilai = '$nilai' WHERE Nim='$nim'";
     $result = $db->query($query);
     if (!$result) {
         die("Could not query the database: <br />" . $db->error);
