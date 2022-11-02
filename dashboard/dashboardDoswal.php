f5b2286aa5c90f7168781fd65f67080b12118c8e
MKMaarif <maarif2910@students.undip.ac.id>
2022-11-02 23:31:46 +0700

    dashboard doswal

--- dashboard/dashboardDoswal.php
+++ dashboard/dashboardDoswal.php
@@ -34,8 +34,9 @@ if (mysqli_num_rows($result) > 0) {
                     <div class="chart-pkl border rounded p-1">
                         <h5 class="card-title">Data Mahasiswa PKL</h5>
                         <?php
-                        if ($stmt = $db->query("SELECT Angkatan, COUNT(*) AS total FROM tb_mhs GROUP BY Angkatan")) {
-                            $sum = $db->query("SELECT COUNT(*) AS total FROM tb_mhs");
+                        $kodewali =  $_SESSION['dataDoswal']['Kode_Wali'];
+                        if ($stmt = $db->query("SELECT p.Status, COUNT(*) AS total FROM tb_pkl p JOIN tb_mhs m ON p.Nim = m.Nim WHERE m.Kode_Wali = '$kodewali' GROUP BY p.Status")) {
+                            $sum = $db->query("SELECT COUNT(*) AS total FROM tb_pkl p JOIN tb_mhs m ON p.Nim = m.Nim WHERE m.Kode_Wali = '$kodewali'");
                             $row = mysqli_fetch_assoc($sum);
                             echo "Total : " . $row['total'] . "<br>";
                             $php_data_array = array(); // create PHP array
@@ -46,11 +47,11 @@ if (mysqli_num_rows($result) > 0) {
                             echo $db->error;
                         }
                         echo "<script>
-                                                    var my_2d = " . json_encode($php_data_array) . "
-                                                    for(i = 0; i < my_2d.length; i++){
-                                                        data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
-                                                    }
-                                                </script>";
+                                var my_2d = " . json_encode($php_data_array) . "
+                                for(i = 0; i < my_2d.length; i++){
+                                    data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
+                                }
+                            </script>";
                         ?>
                         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
                         <script>
@@ -99,23 +100,23 @@ if (mysqli_num_rows($result) > 0) {
                 <div class="chart-aktif border rounded p-3">
                     <h5 class="card-title">Data Mahasiswa Aktif</h5>
                     <?php
-                    if ($stmt = $db->query("SELECT Angkatan, COUNT(*) AS total FROM tb_mhs GROUP BY Angkatan")) {
-                        $sum = $db->query("SELECT COUNT(*) AS total FROM tb_mhs");
+                    if ($stmt = $db->query("SELECT Angkatan, COUNT(*) AS total FROM tb_mhs WHERE Kode_Wali = '$kodewali' GROUP BY Angkatan")) {
+                        $sum = $db->query("SELECT COUNT(*) AS total FROM tb_mhs WHERE Kode_Wali = '$kodewali'");
                         $row = mysqli_fetch_assoc($sum);
                         echo "Total : " . $row['total'] . "<br>";
-                        $php_data_array = array(); // create PHP array
+                        $array_mhs_active = array(); // create PHP array
                         while ($row = $stmt->fetch_row()) {
-                            $php_data_array[] = $row; // Adding to array
+                            $array_mhs_active[] = $row; // Adding to array
                         }
                     } else {
                         echo $db->error;
                     }
                     echo "<script>
-                                            var my_2d = " . json_encode($php_data_array) . "
-                                            for(i = 0; i < my_2d.length; i++){
-                                                data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
-                                            }
-                                        </script>";
+                            var act_diagram = " . json_encode($array_mhs_active) . "
+                            for(i = 0; i < act_diagram.length; i++){
+                                data.addRow([act_diagram[i][0], parseInt(act_diagram[i][1])]);
+                            }
+                        </script>";
                     ?>
                     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
                     <script>
@@ -130,8 +131,8 @@ if (mysqli_num_rows($result) > 0) {
                             var data = new google.visualization.DataTable();
                             data.addColumn('string', 'Angkatan');
                             data.addColumn('number', 'Total');
-                            for (i = 0; i < my_2d.length; i++)
-                                data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
+                            for (i = 0; i < act_diagram.length; i++)
+                                data.addRow([act_diagram[i][0], parseInt(act_diagram[i][1])]);
                             // above row adds the JavaScript two dimensional array data into required chart format
                             var options = {
                                 pieHole: 0.5,
