d4fabd3d5d504031e94baf4992f5bd045f0b8d8b
Muhammad Farhan Haris Rosidi <farhanharis66@gmail.com>
2022-11-16 23:45:40 +0700

    Rapiin Dashboard Doswal dan OP

--- dashboard/dashboardDoswal.php
+++ dashboard/dashboardDoswal.php
@@ -14,89 +14,94 @@ if (mysqli_num_rows($result) > 0) {
 ?>
 <div class="main h-100">
     <div class="title-page text-center display-6 mt-3 fw-semibold">Dashboard Dosen Wali</div>
-    <div class="content d-flex align-items-center ">
-        <div class="left-content col-5">
-            <div class="lurus d-flex justify-content-center">
-                <div class="profile border rounded p-3 w-75">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex justify-content-center">
                     <div>
-                        <img class="img-thumbnail rounded" src="../lib/pp.jpg" alt="profile">
-                    </div>
-                    <div class="profile-name text-center">
-                        <h3><?php echo $_SESSION["dataDoswal"]["Nama"] ?></h3>
-                        <p>Dosen Wali</p>
-                        <p><?= $nip?></p>
+                        <div class="profile border rounded p-3 h-100 w-100">
+                            <div>
+                                <img class="img-fluid" src="../lib/pp.jpg" alt="profile">
+                            </div>
+                            <div class="profile-name text-center">
+                                <h4 class="mt-1"><?php echo $_SESSION["dataDoswal"]["Nama"] ?></h4>
+                                <h5 class="mt-1">Dosen Wali</h5>
+                                <h6 class="mt-1"><?= $nip ?></h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <!-- Donut Chart Jumlah Mahasiswa -->
-            <div class="lurus d-flex justify-content-center my-3">
-    
-                    <div class="chart-pkl border rounded p-1">
-                        <h5 class="card-title">Data Mahasiswa PKL</h5>
-                        <?php
-                        $kodewali =  $_SESSION['dataDoswal']['Kode_Wali'];
-                        if ($stmt = $db->query("SELECT p.Status, COUNT(*) AS total FROM tb_pkl p JOIN tb_mhs m ON p.Nim = m.Nim WHERE m.Kode_Wali = '$kodewali' GROUP BY p.Status")) {
-                            $sum = $db->query("SELECT COUNT(*) AS total FROM tb_pkl p JOIN tb_mhs m ON p.Nim = m.Nim WHERE m.Kode_Wali = '$kodewali'");
-                            $row = mysqli_fetch_assoc($sum);
-                            echo "Total : " . $row['total'] . "<br>";
-                            $array_pkl_done = array(); // create PHP array
-                            while ($row = $stmt->fetch_row()) {
-                                $array_pkl_done[] = $row; // Adding to array
-                            }
-                        } else {
-                            echo $db->error;
+            <div class="col text-center">
+                <div class="chart-pkl border rounded p-3 h-100">
+                    <h5 class="card-title">Data Mahasiswa PKL</h5>
+                    <?php
+                    $kodewali =  $_SESSION['dataDoswal']['Kode_Wali'];
+                    if ($stmt = $db->query("SELECT p.Status, COUNT(*) AS total FROM tb_pkl p JOIN tb_mhs m ON p.Nim = m.Nim WHERE m.Kode_Wali = '$kodewali' GROUP BY p.Status")) {
+                        $sum = $db->query("SELECT COUNT(*) AS total FROM tb_pkl p JOIN tb_mhs m ON p.Nim = m.Nim WHERE m.Kode_Wali = '$kodewali'");
+                        $row = mysqli_fetch_assoc($sum);
+                        echo "Total : " . $row['total'] . "<br>";
+                        $array_pkl_done = array(); // create PHP array
+                        while ($row = $stmt->fetch_row()) {
+                            $array_pkl_done[] = $row; // Adding to array
                         }
-                        echo "<script>
+                    } else {
+                        echo $db->error;
+                    }
+                    echo "<script>
                                 var my_1d = " . json_encode($array_pkl_done) . "
                                 for(i = 0; i < my_1d.length; i++){
                                     data.addRow([my_1d[i][0], parseInt(my_1d[i][1])]);
                                 }
                             </script>";
-                        ?>
-                        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-                        <script>
-                            google.charts.load('current', {
-                                'packages': ['corechart']
-                            });
-                            // Draw the pie chart when Charts is loaded.
-                            google.charts.setOnLoadCallback(draw_my_chart);
-                            // Callback that draws the pie chart
-                            function draw_my_chart() {
-                                // Create the data table .
-                                var data = new google.visualization.DataTable();
-                                data.addColumn('string', 'Angkatan');
-                                data.addColumn('number', 'Total');
-                                for (i = 0; i < my_1d.length; i++)
-                                    data.addRow([my_1d[i][0], parseInt(my_1d[i][1])]);
-                                // above row adds the JavaScript two dimensional array data into required chart format
-                                var options = {
-                                    pieHole: 0.5,
-                                    'width': 400,
-                                    'height': 400,
-                                    'legend': 'none',
-                                    'alignment': 'center',
-                                    'chartArea': {
-                                        'width': '100%',
-                                        'height': '90%'
-                                    },
-                                    'pieSliceText': 'label',
-                                    'pieSliceTextStyle': {
-                                        'color': 'white',
-                                    },
-                                    'backgroundColor': 'transparent',
-                                };
-                                // Instantiate and draw the chart
-                                var chart = new google.visualization.PieChart(document.getElementById('totalChart'));
-                                chart.draw(data, options);
-                            }
-                        </script>
-                        <div id="totalChart"></div>
+                    ?>
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script>
+                        google.charts.load('current', {
+                            'packages': ['corechart']
+                        });
+                        // Draw the pie chart when Charts is loaded.
+                        google.charts.setOnLoadCallback(draw_my_chart);
+                        // Callback that draws the pie chart
+                        function draw_my_chart() {
+                            // Create the data table .
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'Angkatan');
+                            data.addColumn('number', 'Total');
+                            for (i = 0; i < my_1d.length; i++)
+                                data.addRow([my_1d[i][0], parseInt(my_1d[i][1])]);
+                            // above row adds the JavaScript two dimensional array data into required chart format
+                            var options = {
+                                pieHole: 0.3,
+                                'width': 400,
+                                'height': 400,
+                                'legend': 'none',
+                                'alignment': 'center',
+                                'chartArea': {
+                                    'width': '100%',
+                                    'height': '90%'
+                                },
+                                'pieSliceText': 'label',
+                                'pieSliceTextStyle': {
+                                    'color': 'white',
+                                },
+                                'backgroundColor': 'transparent',
+                            };
+                            // Instantiate and draw the chart
+                            var chart = new google.visualization.PieChart(document.getElementById('totalChart'));
+                            chart.draw(data, options);
+                        }
+                    </script>
+                    <div class="d-flex justify-content-center">
+                        <div class="mx-auto" id="totalChart"></div>
                     </div>
+                </div>
             </div>
         </div>
-        <div class="right-content col d-flex flex-column">
+        <div class="row mt-4">
             <!-- Donut Chart Jumlah Mahasiswa -->
-            <div class="lurus d-flex justify-content-center mt-5 mb-3">
+            <div class="col text-center">
                 <div class="chart-aktif border rounded p-3">
                     <h5 class="card-title">Data Mahasiswa Aktif</h5>
                     <?php
@@ -135,7 +140,7 @@ if (mysqli_num_rows($result) > 0) {
                                 data.addRow([act_diagram[i][0], parseInt(act_diagram[i][1])]);
                             // above row adds the JavaScript two dimensional array data into required chart format
                             var options = {
-                                pieHole: 0.5,
+                                pieHole: 0.3,
                                 'width': 400,
                                 'height': 400,
                                 'legend': 'none',
@@ -155,12 +160,14 @@ if (mysqli_num_rows($result) > 0) {
                             chart.draw(data, options);
                         }
                     </script>
-                    <div id="totalChartAktif"></div>
+                    <div class="d-flex justify-content-center">
+                        <div id="totalChartAktif"></div>
+                    </div>
                 </div>
             </div>
 
 
-            <div class="lurus d-flex justify-content-center mb-3">
+            <div class="col text-center">
 
                 <div class="chart-skripsi border rounded p-3">
                     <h5 class="card-title">Data Mahasiswa Skripsi</h5>
@@ -188,41 +195,42 @@ if (mysqli_num_rows($result) > 0) {
                     <script>
                         google.charts.load('current', {
                             'packages': ['corechart']
-                        }
-                        );
+                        });
                         // Draw the pie chart when Charts is loaded.
                         google.charts.setOnLoadCallback(draw_my_chart);
                         // Callback that draws the pie chart
                         function draw_my_chart() {
-                        // Create the data table .
-                        var data = new google.visualization.DataTable();
-                        data.addColumn('string', 'Angkatan');
-                        data.addColumn('number', 'Total');
-                        for (i = 0; i < my_3d.length; i++)
-                            data.addRow([my_3d[i][0], parseInt(my_3d[i][1])]);
-                                // above row adds the JavaScript two dimensional array data into required chart format
-                                var options = {
-                                    pieHole: 0.5,
-                                    'width': 400,
-                                    'height': 400,
-                                    'legend': 'none',
-                                    'alignment': 'center',
-                                    'chartArea': {
-                                        'width': '100%',
+                            // Create the data table .
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'Angkatan');
+                            data.addColumn('number', 'Total');
+                            for (i = 0; i < my_3d.length; i++)
+                                data.addRow([my_3d[i][0], parseInt(my_3d[i][1])]);
+                            // above row adds the JavaScript two dimensional array data into required chart format
+                            var options = {
+                                pieHole: 0.3,
+                                'width': 400,
+                                'height': 400,
+                                'legend': 'none',
+                                'alignment': 'center',
+                                'chartArea': {
+                                    'width': '100%',
                                     '   height': '90%'
-                                    },
-                                    'pieSliceText': 'label',
-                                    'pieSliceTextStyle': {
-                                        'color': 'white',
-                                    },
+                                },
+                                'pieSliceText': 'label',
+                                'pieSliceTextStyle': {
+                                    'color': 'white',
+                                },
                                 '   backgroundColor': 'transparent',
-                                };
-                                // Instantiate and draw the chart
-                                var chart = new google.visualization.PieChart(document.getElementById('totalChartSkripsi'));
-                                chart.draw(data, options);
+                            };
+                            // Instantiate and draw the chart
+                            var chart = new google.visualization.PieChart(document.getElementById('totalChartSkripsi'));
+                            chart.draw(data, options);
                         }
                     </script>
-                    <div id="totalChartSkripsi"></div>
+                    <div class="d-flex justify-content-center">
+                        <div id="totalChartSkripsi"></div>
+                    </div>
                 </div>
             </div>
         </div>
--- dashboard/dashboardOp.php
+++ dashboard/dashboardOp.php
@@ -16,73 +16,71 @@ $result3 = mysqli_query($db, $query3);
 
 ?>
 <div class="container">
-    <h3 class="d-flex justify-content-center mt-3">Dashboard Operator</h3>
-    <div class="row">
-        <div class="col">
+    <h3 class="d-flex justify-content-center mt-3 fw-semibold display-6">Dashboard Operator</h3>
+    <div class="row mt-5">
+        <div class="col d-flex align-item-center justify-content-center">
             <div class="card">
                 <div class="card-body">
-                <h3 class= "card-title">Data Mahasiswa</h3>
+                    <h3 class="card-title text-center">Data Mahasiswa</h3>
                     <div class="row">
-                            <div class="second-content">
-                                <div class="tengah d-flex justify-content-center">
-                                    <div class="chart col text-center ml-5">
-                                        <?php
-                                        if ($stmt = $db->query("SELECT Angkatan, COUNT(*) AS total FROM tb_mhs GROUP BY Angkatan")) {
-                                            $sum = $db->query("SELECT COUNT(*) AS total FROM tb_mhs");
-                                            $php_data_array = array(); // create PHP array
-                                            while ($row = $stmt->fetch_row()) {
-                                                $php_data_array[] = $row; // Adding to array
-                                            }
-                                        } else {
-                                            echo $db->error;
-                                        }
-                                        echo "<script>
+                        <div class="tengah d-flex justify-content-center">
+                            <div class="chart col text-center ml-5">
+                                <?php
+                                if ($stmt = $db->query("SELECT Angkatan, COUNT(*) AS total FROM tb_mhs GROUP BY Angkatan")) {
+                                    $sum = $db->query("SELECT COUNT(*) AS total FROM tb_mhs");
+                                    $php_data_array = array(); // create PHP array
+                                    while ($row = $stmt->fetch_row()) {
+                                        $php_data_array[] = $row; // Adding to array
+                                    }
+                                } else {
+                                    echo $db->error;
+                                }
+                                echo "<script>
                                                     var my_2d = " . json_encode($php_data_array) . "
                                                     for(i = 0; i < my_2d.length; i++){
                                                         data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
                                                     }
                                                 </script>";
-                                        ?>
-                                        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-                                        <script>
-                                            google.charts.load('current', {
-                                                'packages': ['corechart']
-                                            });
-                                            // Draw the pie chart when Charts is loaded.
-                                            google.charts.setOnLoadCallback(draw_my_chart);
-                                            // Callback that draws the pie chart
-                                            function draw_my_chart() {
-                                                // Create the data table .
-                                                var data = new google.visualization.DataTable();
-                                                data.addColumn('string', 'Angkatan');
-                                                data.addColumn('number', 'Total');
-                                                for (i = 0; i < my_2d.length; i++)
-                                                    data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
-                                                // above row adds the JavaScript two dimensional array data into required chart format
-                                                var options = {
-                                                    pieHole: 0.5,
-                                                    'width': 500,
-                                                    'height': 500,
-                                                    'legend': 'none',
-                                                    'alignment': 'center',
-                                                    'chartArea': {
-                                                        'width': '100%',
-                                                        'height': '90%'
-                                                    },
-                                                    'pieSliceText': 'label',
-                                                    'pieSliceTextStyle': {
-                                                        'color': 'white',
-                                                    },
-                                                    'backgroundColor': 'transparent',
-                                                };
-                                                // Instantiate and draw the chart
-                                                var chart = new google.visualization.PieChart(document.getElementById('totalChart'));
-                                                chart.draw(data, options);
-                                            }
-                                        </script>
-                                        <div class="d-flex justify-content-center">
-                                            <div  id="totalChart"></div>
-                                        </div>
+                                ?>
+                                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                                <script>
+                                    google.charts.load('current', {
+                                        'packages': ['corechart']
+                                    });
+                                    // Draw the pie chart when Charts is loaded.
+                                    google.charts.setOnLoadCallback(draw_my_chart);
+                                    // Callback that draws the pie chart
+                                    function draw_my_chart() {
+                                        // Create the data table .
+                                        var data = new google.visualization.DataTable();
+                                        data.addColumn('string', 'Angkatan');
+                                        data.addColumn('number', 'Total');
+                                        for (i = 0; i < my_2d.length; i++)
+                                            data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
+                                        // above row adds the JavaScript two dimensional array data into required chart format
+                                        var options = {
+                                            pieHole: 0.5,
+                                            'width': 400,
+                                            'height': 400,
+                                            'legend': 'none',
+                                            'alignment': 'center',
+                                            'chartArea': {
+                                                'width': '100%',
+                                                'height': '90%'
+                                            },
+                                            'pieSliceText': 'label',
+                                            'pieSliceTextStyle': {
+                                                'color': 'white',
+                                            },
+                                            'backgroundColor': 'transparent',
+                                        };
+                                        // Instantiate and draw the chart
+                                        var chart = new google.visualization.PieChart(document.getElementById('totalChart'));
+                                        chart.draw(data, options);
+                                    }
+                                </script>
+                                <div class="d-flex justify-content-center align-item-center">
+                                    <div id="totalChart"></div>
                                 </div>
                             </div>
                         </div>
@@ -91,35 +89,40 @@ $result3 = mysqli_query($db, $query3);
             </div>
         </div>
         <div class="col">
-            <div class="card">
-                <div class="card-body text-center">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" width="150" alt="gene">
-                    <h3 class="m-0"><?php echo $_SESSION["user"]["Username"] ?></h3>
-                    <p class="m-0">Operator</p>
-                </div>
-            </div>
-            <div class="row mt-3"></div>
-            <div class="card">
-                <div class="card-body">
-                <h3 class="card-title text-center" class="m">Data User</h3>
-                    <div class= "row">
-                        <div class="col">
-                            <p class="text-center m-0">Mahasiswa</p>
-                            <h3 class="text-center"><?php echo mysqli_num_rows($result1); ?></h3>
+            <div class="d-flex align-item-center justify-content-center">
+
+                <div class="align">
+                    <div class="card w-100">
+                        <div class="card-body text-center">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" width="150" alt="gene">
+                            <h2><?php echo $_SESSION["user"]["Username"] ?></h2>
+                            <h5>Operator</h5>
                         </div>
-                        <div class="col">
-                            <p class="text-center m-0">Dosen</p>
-                            <h3 class="text-center"><?php echo mysqli_num_rows($result2); ?></h3>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <p class="text-center m-0">Total User</p>
-                                <h3 class="text-center"><?php echo mysqli_num_rows($result3); ?></h3>
+                    </div>
+                    <div class="row mt-3"></div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title text-center">Data User</h3>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="text-center m-0">Mahasiswa</div>
+                                    <h3 class="text-center"><?php echo mysqli_num_rows($result1); ?></h3>
+                                </div>
+                                <div class="col">
+                                    <div class="text-center m-0">Dosen</div>
+                                    <h3 class="text-center"><?php echo mysqli_num_rows($result2); ?></h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="text-center ">Total User</div>
+                                        <h3 class="text-center"><?php echo mysqli_num_rows($result3); ?></h3>
+                                    </div>
+                                </div>
+                                <div class="text-center mt-4">
+                                    <a href="../operator/addUserPage.php" class="btn btn-primary px-3 py-2">Add User</a>
+                                </div>
                             </div>
                         </div>
-                        <div class="text-center mt-4">
-                            <a href="../operator/addUserPage.php" class="btn btn-primary px-3 py-2">Add User</a>
-                        </div>     
                     </div>
                 </div>
             </div>
