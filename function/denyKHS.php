17ead8ba53e4ee1b9af9f801515b9bc1dc20e005
abyanardiatama <abyanardiatama@students.undip.ac.id>
2022-11-24 13:42:30 +0700

    KHS Approval

---
+++ function/denyKHS.php
@@ -0,0 +1,14 @@
+<?php
+    require_once('../lib/db_login.php');
+
+    $nim = $_POST['nim'];
+    $smt = $_POST['smt'];
+
+    
+    $query1 = "UPDATE tb_khs SET Status='Belum Disetujui' WHERE Nim = '$nim' AND Semester = '$smt'";
+    $result1 = $db->query($query1);
+    if (!$result1) {
+        die("Could not query the database: <br />" . $db->error);
+    } else {
+        echo '<div id="setujui"  class="alert alert-danger" role="alert">KHS Batal Disetujui</div>';;   
+    }
