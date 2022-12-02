4b614d3918e87888d1242dfda58877c4c222adf9
abyanardiatama <abyanardiatama@students.undip.ac.id>
2022-12-02 13:18:15 +0700

    dashboard mahasiswa fix

--- dashboard/dashboardMhs.php
+++ dashboard/dashboardMhs.php
@@ -29,7 +29,7 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
     <h1 class="d-flex justify-content-center mt-4 mb-3">Dashboard Mahasiswa</h1>
     <div class="row">
         <div class="col">
-            <div class="card me-5 mt-3 mb-2 w-100 h-100">
+            <div class="card ms-2 mt-3 mb-2 w-100 h-100">
                 <div class="d-flex justify-content-center align-middle">
                     <div class="card-body mt-3">
                         <h4 class="card-title mb-3">Status Akademik</h4>
@@ -39,19 +39,19 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
                         <p class="fw-bold m-0"><?php echo $resultDoswal->nip ?></p>
                         <div class="semesternstatus mx-2">
                             <div class="row text-center mt-4">
-                                <div class="col">Semester</div>
-                                <div class="col">Status</div>
+                                <div class="fs-5 fw-bold col">Semester</div>
+                                <div class="fs-5 fw-bold col">Status</div>
                             </div>
-                            <div class="row text-center">
-                                <h1 class="col"><?php echo $_SESSION["dataMhs"]["Semester"] ?></h1>
+                            <div class="text-center row text-center">
+                                <h1 class="mt-2 col-6"><?php echo $_SESSION["dataMhs"]["Semester"] ?></h1>
                                 <?php
                                 if ($_SESSION['dataMhs']['Status'] == "Aktif") {
-                                    echo '<h4 class="col w-25 badge fs-4 text-bg-success text-white">'.$_SESSION["dataMhs"]["Status"].'</h4>';
+                                    echo '<h4 class="m-auto col-4 badge fs-3 text-bg-success text-white">'.$_SESSION["dataMhs"]["Status"].'</h4>';
                                 } else if($_SESSION['dataMhs']['Status'] == "Cuti"){ 
-                                    echo '<h4 class="col w-25 badge fs-4 text-bg-primary text-white">'.$_SESSION["dataMhs"]["Status"].'</h4>';
+                                    echo '<h4 class="m-auto col-4 badge fs-4 text-bg-primary text-white">'.$_SESSION["dataMhs"]["Status"].'</h4>';
                                 }
                                 else{
-                                    echo '<h4 class="col w-25 badge fs-4 text-bg-danger text-white">'.$_SESSION["dataMhs"]["Status"].'</h4>';
+                                    echo '<h4 class="m-auto col-4 badge fs-4 text-bg-danger text-white">'.$_SESSION["dataMhs"]["Status"].'</h4>';
                                 }
                                 ?>
                                 
@@ -61,8 +61,8 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card mt-3 mb-2 me-4 w-100 h-100">
+        <div class="col me-4">
+            <div class="card mt-3 mb-2 me-5 w-100 h-100">
                 <div class="card-body text-center">
                 <img class="img-thumbnail rounded p-0 mt-4" src="../lib/pp.jpg" alt="profile" width="130">
                     <h3 class="mt-3"><?php echo $_SESSION["dataMhs"]["Nama"] ?></h3>
@@ -73,10 +73,10 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
         </div>
     </div>
     <div class="row mt-3">
-        <div class="col">
+        <div class="col ms-2">
             <div class="card mt-3 w-100 h-100">
                 <div class="card-body">
-                    <h4>Data Mahasiswa</h4>
+                    <h4 class="mt-2">Data Mahasiswa</h4>
                     <div class="mt-3">
                         <p class="m-0">Nama Lengkap</p>
                         <p class="fw-bold m-0"><?php echo $_SESSION["dataMhs"]["Nama"] ?></p>
@@ -86,16 +86,16 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
                         <div class="d-flex justify-content-between">
                             <!-- anchor editProfil -->
                                 <p class="fw-bold"><?php echo $_SESSION["dataMhs"]["Nim"] ?></p>
-                            <a href="../mahasiswa/editProfilMhsPage.php" class="btn btn-primary">Edit Profil</a>
+                            <a href="../mahasiswa/editProfilMhsPage.php" class="me-2 btn btn-primary">Edit Profil</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col">
+        <div class="col me-4 ">
             <div class="card mt-3 w-100 h-100">
                 <div class="card-body">
-                    <h4 class="card-title text-center mb-3">Prestasi Akademik</h4>
+                    <h4 class="card-title text-center mt-2 mb-3">Prestasi Akademik</h4>
                     <div class="row">
                         <div class="col mb-1">
                             <p class="text-center m-0">SKS semester</p>
@@ -111,9 +111,9 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
                             <p class="text-center m-0">IP semester</p>
                             <h3 class="text-center mb-4"><?php echo $_SESSION['dataKhs']['Ip'];?></h3>
                         </div>
-                        <div class="col">
+                        <div class="mb-2 col">
                             <p class="text-center m-0">IP Kumulatif</p>
-                            <h3 class="text-center mb-4"><?php echo $_SESSION['dataKhs']['Ip_Kumulatif'];?></h3>
+                            <h3 class="text-center mb-2"><?php echo $_SESSION['dataKhs']['Ip_Kumulatif'];?></h3>
                         </div>
                     </div>
                 </div>
@@ -121,6 +121,7 @@ $resultDoswal = $db->query($queryDoswal)->fetch_object();
         </div>
     </div>
 </div>
+<div class="mt-5 mb-5"></div>
 
 <script>
     const namaLengkap = document.getElementById('namaLengkap');
