9e89c5ffb5ce09d21feacfa50777e0faecbe99fe
Muhammad Farhan Haris Rosidi <farhanharis66@gmail.com>
2022-12-02 14:00:31 +0700

    new login page design

--- auth/login.php
+++ auth/login.php
@@ -25,58 +25,36 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-<div class="app-content content ">
-    <div class="content-wrapper">
-        <div class="content-header row">
-        </div>
-        <div class="content-body">
-            <section class="flexbox-container" style="overflow: auto;">
-                <div class="col-12 d-flex align-items-center justify-content-center">
-                    <div class="col-xl-4 col-lg-6 col-md-12 col-sm-12 box-shadow-2 p-0">
-                        <div class="card border-grey border-lighten-3 px-1 py-1 mt-5">
-                            <div class="card-header border-0 pb-0 bg-primary">
-                                <div class="card-title text-center">
-                                    <img src="https://sso.undip.ac.id/assets/app/images/logo-undip.png" alt="Universitas Diponegoro" style="width: 150px;">
-                                    <h1 class="text-white"><strong>SIAP</strong></h1>
-                                    <h2 class="text-white">Informatika Undip</h2> 
-                                </div>
-                            </div>
-                            <div class="card-content">
-                                <div class="card-body">
 
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-xs-12 mb-1">
-                                            <marquee onmouseover="this.stop();" onmouseout="this.start();">
-                                                Selamat Datang di Sistem Informasi Akademik Informatika Universitas Diponegoro
-                                            </marquee>
-                                        </div>
-                                    </div>
-                                    <form class="form-horizontal" action="login.php" method="post" novalidate="">
-                                        <fieldset class="form-group position-relative has-icon-left  wrapper_identity mb-3">
-                                            <label for="username" class="fw-bold">Username</label>
-                                            <input type="text" class="form-control" id="username" name="username" placeholder="NIM/NIP/username/e-mail official Undip" required value="<?php if(isset($_POST["username"])) echo $username?>">
-                                        </fieldset>
-                                        <div class="form-group">
-                                            <label for="password" class="fw-bold" >Password</label>
-                                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                                        </div>
-                                        <?php if (isset($error)) : ?>
-                                            <div class="alert alert-danger mt-3" role="alert">
-                                                <?php echo $error; ?>
-                                            </div>
-                                        <?php endif; ?>
-                                        <div class="text-center mt-4">
-                                            <button type="submit" name="submit" value="submit" class="btn btn-primary w-100">Login</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<div class="content-body" style="height:100vh; background-image: url('../lib/background.jpg')">
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh ;">
+        <div class="login mb-5" style="width: 20%;">
+            <div class="card-header">
+                <div class="card-title text-center mb-4">
+                    <img src="https://sso.undip.ac.id/assets/app/images/logo-undip.png" alt="Universitas Diponegoro" style="width: 70%;">
+                    <h2 class="text-black"><strong>SIAP</strong></h2>
+                    <h3 class="text-black fw-bold">Informatika Undip</h3>
                 </div>
-            </section>
-
+            </div>
+            <form class="form-horizontal" action="login.php" method="post" novalidate="">
+                <fieldset class="form-group position-relative has-icon-left  wrapper_identity mb-3">
+                    <label for="username" class="fw-bold">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" placeholder="Username" required value="<?php if (isset($_POST["username"])) echo $username ?>">
+                </fieldset>
+                <div class="form-group">
+                    <label for="password" class="fw-bold">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                </div>
+                <?php if (isset($error)) : ?>
+                    <div class="alert alert-danger mt-3" role="alert">
+                        <?php echo $error; ?>
+                    </div>
+                <?php endif; ?>
+                <div class="text-center mt-4">
+                    <button type="submit" name="submit" value="submit" class="btn btn-primary w-100 fw-semibold">Login</button>
+                </div>
+            </form>
         </div>
-    </div>
+    </div >
 </div>
 <?php require_once("../bootstrap/header.html") ?>
